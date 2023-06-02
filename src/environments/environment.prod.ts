f65f74d4ced0e369a3213b73378b2d06d9e653b2
juaozinpjl <125033520+juaozinpjl@users.noreply.github.com>
2023-06-02 13:36:15 +0000

    Tela de login e registro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+  firebaseConfig : {
+    apiKey: "AIzaSyC5l6ODW3cD8PTnLGgcVt6vppQO0hXaGBM",
+    authDomain: "aula-2bc5b.firebaseapp.com",
+    projectId: "aula-2bc5b",
+    storageBucket: "aula-2bc5b.appspot.com",
+    messagingSenderId: "806253626377",
+    appId: "1:806253626377:web:036062c18dd9c77df3a4d8",
+    measurementId: "G-037R1Q3M46"
   },
   production: true
 };
